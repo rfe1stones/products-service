54a45bebeac8796df08d6fc04fc479321d0a93f4
Stephen Hyde <shh2112@gmail.com>
2021-03-20 12:45:36 -0400

    write query for styles endpoint

--- src/routes/product-styles.ts
+++ src/routes/product-styles.ts
@@ -7,9 +7,32 @@ const productStylesHandler = (req: Request, res: Response) => {
   const productId = Number(req.params.id);
   connection.then((conn) => {
     if (!conn) { throw Error('Connection not established!'); }
-    res.json({
-      data: productId
-    });
+    const sql = `
+      SELECT
+        s.style_id,
+        s.name,
+        s.original_price,
+        s.sale_price,
+        s.default_style,
+        GROUP_CONCAT(sp.thumbnail_url) as thumbnails,
+        GROUP_CONCAT(sp.url) as photos,
+        GROUP_CONCAT(sk.sku_id, ',', sk.size,',', sk.quantity SEPARATOR ';') as skus
+      FROM styles s
+        LEFT JOIN style_photos sp
+          ON s.style_id = sp.style_id
+        LEFT JOIN skus sk
+          ON s.style_id = sk.style_id
+        WHERE s.product_id = ?
+        GROUP BY s.style_id
+    `;
+    conn.query(sql, [productId])
+      .then((results) => {
+        res.json(results);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+
   });
 };
 
