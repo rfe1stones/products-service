a03bec8453102213609f4e5c44a6e0c3324c5e80
Stephen Hyde <shh2112@gmail.com>
2021-03-20 11:00:15 -0400

    write query to pull data for single product

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -5,11 +5,11 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 const connection = mariadb.createConnection({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    port: parseInt(process.env.DB_PORT as string),
-    socketPath: process.env.DB_SOCKET_PATH,
-    database: process.env.DB_NAME
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  port: parseInt(process.env.DB_PORT as string),
+  socketPath: process.env.DB_SOCKET_PATH,
+  database: process.env.DB_NAME
 }).catch((err) => {
   console.log(err);
 });
@@ -22,7 +22,16 @@ router.get('/:id', (req: Request, res: Response) => {
     if (!conn) {
       throw Error('Connection not established');
     }
-    conn.query('SELECT * FROM products WHERE product_id = ?', [productId])
+    const sql = `
+      SELECT
+        p.product_id as id, p.name, p.slogan, p.description, p.category, p.default_price,
+        f.feature, f.value
+      FROM products p
+      LEFT JOIN features f
+      ON p.product_id = f.product_id
+      WHERE p.product_id = ?
+    `;
+    conn.query(sql, [productId])
       .then((result) => {
         res.json(result);
       })
